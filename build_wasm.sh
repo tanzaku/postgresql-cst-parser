5e545252cc41dff5c78bf56de8e033f68dd0ecb7
tanzaku <tanzaku@users.noreply.github.com>
2024-02-21 21:55:02 +0900

    docs

--- build_wasm.sh
+++ build_wasm.sh
@@ -4,6 +4,6 @@ set -eux
 
 cd crates/postgresql-cst-parser-wasm
 wasm-pack build --release --target web
-cp pkg/*.js pkg/*.ts pkg/*.wasm dist/js
-cd dist
+cp pkg/*.js pkg/*.ts pkg/*.wasm ../../docs/js
+cd ../../docs
 python3 -m http.server 8000
