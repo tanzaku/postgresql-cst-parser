82bc1061760217a74c4c560f8b2007f4edd91974
tanzaku <tanzaku@users.noreply.github.com>
2024-05-05 10:20:31 +0900

    Update wasm

--- build_wasm.sh
+++ build_wasm.sh
@@ -4,6 +4,7 @@ set -eux
 
 cd crates/postgresql-lst-parser-wasm
 wasm-pack build --release --target web
+rm -rf ./pkg
 cp pkg/*.js pkg/*.ts pkg/*.wasm ../../docs/js
 cd ../../docs
 python3 -m http.server 8000
