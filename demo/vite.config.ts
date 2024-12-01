a7172b9de4991bf557f7a91af62a343631e3f08c
tanzaku <tanzaku@users.noreply.github.com>
2024-12-02 00:28:46 +0900

    fix: vite config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// basePathを構築
+const getBasePath = () => {
+  // GitHub Actionsの場合
+  if (process.env.GITHUB_REF_NAME) {
+    const branch = process.env.GITHUB_REF_NAME === 'main'
+      ? ''
+      : process.env.GITHUB_REF_NAME
+    return `/postgresql-cst-parser/${branch}`
+  }
+  // ローカル開発の場合
+  return '/'
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/postgresql-cst-parser/',
-  base: '/',
+  base: getBasePath(),
   build: {
     outDir: '../docs',
   }
