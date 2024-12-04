b4aed7530142a16deb85b2b8e5c065d118557f53
lemonadern <taishi.develop@gmail.com>
2024-12-04 18:44:12 +0900

    refactor: Simplify the cst-transformation process into a single pure function

--- crates/postgresql-cst-parser/src/tree_sitter/transform.rs
+++ crates/postgresql-cst-parser/src/tree_sitter/transform.rs
@@ -1,86 +1,76 @@
-use cstree::{build::GreenNodeBuilder, green::GreenNode, interning::Interner, Syntax};
+use cstree::{build::GreenNodeBuilder, syntax::SyntaxNode};
 
 use crate::{syntax_kind::SyntaxKind, PostgreSQLSyntax, ResolvedNode};
 
-// TODO(refactor): TreeBuilderにしなくても、(ResolvedNode -> ResolvedNode) な純粋関数でいいかも
-struct TreeBuilder {
-    // lexer がいらない
-    builder: GreenNodeBuilder<'static, 'static, PostgreSQLSyntax>,
+pub fn transform_cst(root: &ResolvedNode) -> ResolvedNode {
+    let mut builder = GreenNodeBuilder::new();
+    builder.start_node(SyntaxKind::Root);
+    walk_and_build(&mut builder, root);
+    builder.finish_node();
+
+    let (tree, cache) = builder.finish();
+    let new_root =
+        SyntaxNode::new_root_with_resolver(tree, cache.unwrap().into_interner().unwrap());
+    new_root
 }
 
-impl TreeBuilder {
-    pub fn new() -> Self {
-        Self {
-            builder: GreenNodeBuilder::new(),
-        }
-    }
+/// CST を走査し、いくつかのNodeを書き換える
+/// e.g. flatten list node, remove option node
+fn walk_and_build(
+    builder: &mut GreenNodeBuilder<'static, 'static, PostgreSQLSyntax>,
+    node: &ResolvedNode,
+) {
+    // for now, just walk
+    use cstree::util::NodeOrToken;
+    let mut children = node.children_with_tokens();
 
-    pub fn build(&mut self, node: &ResolvedNode) -> Result<(), String> {
-        self.builder.start_node(SyntaxKind::Root);
-        self.walk_and_build(node)?;
-        self.builder.finish_node();
-        Ok(())
-    }
-
-    fn walk_and_build(&mut self, node: &ResolvedNode) -> Result<(), String> {
-        // for now, just walk
-        use cstree::util::NodeOrToken;
-        let mut children = node.children_with_tokens();
-
-        while let Some(child) = children.next() {
-            match child {
-                NodeOrToken::Node(n) => {
-                    // for debug
-                    if format!("{}", n.kind()).starts_with("opt_") {
-                        println!("Node  (kind: {})", n.kind());
-                    }
-
-                    // thinking: opt_* でマッチして処理するか、match で全列挙するか？
-                    //     node.kind って Enum か Raw Value しかないから、前方一致で分岐させるのは厳しいか
-                    //     format!("{}", n.kind()).starts_with("opt_") とはできるけど、これってオーバーヘッドないのか？
-                    //     tree_sitter::is_flattern_all では列挙してるからそれに従おう
-
-                    match n.kind() {
-                        // TODO:
-                        // SyntaxKind::target_list=> {},
-
-                        // all pattern
-                        kind @ _ => {
-                            self.builder.start_node(kind);
-
-                            self.walk_and_build(n)?;
-
-                            self.builder.finish_node();
-                        }
-                    }
+    while let Some(child) = children.next() {
+        match child {
+            NodeOrToken::Node(n) => {
+                // for debug
+                if format!("{}", n.kind()).starts_with("opt_") {
+                    println!("Node  (kind: {})", n.kind());
                 }
-                NodeOrToken::Token(t) => {
-                    // for debug
-                    // println!(
-                    //     "Token (kind: {}, text: \"{}\")",
-                    //     t.kind(),
-                    //     t.text().escape_debug()
-                    // );
 
-                    self.builder.token(t.kind(), t.text());
+                // thinking: opt_* でマッチして処理するか、match で全列挙するか？
+                //     node.kind って Enum か Raw Value しかないから、前方一致で分岐させるのは厳しいか
+                //     format!("{}", n.kind()).starts_with("opt_") とはできるけど、これってオーバーヘッドないのか？
+                //     tree_sitter::is_flattern_all では列挙してるからそれに従おう
+
+                match n.kind() {
+                    // TODO:
+                    // SyntaxKind::target_list=> {},
+
+                    // all pattern
+                    kind @ _ => {
+                        builder.start_node(kind);
+
+                        walk_and_build(builder, n);
+
+                        builder.finish_node();
+                    }
                 }
             }
-        }
-        Ok(())
-    }
+            NodeOrToken::Token(t) => {
+                // for debug
+                // println!(
+                //     "Token (kind: {}, text: \"{}\")",
+                //     t.kind(),
+                //     t.text().escape_debug()
+                // );
 
-    pub fn finish(self) -> (GreenNode, impl Interner) {
-        let (tree, cache) = self.builder.finish();
-        (tree, cache.unwrap().into_interner().unwrap())
+                builder.token(t.kind(), t.text());
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{cst, tree_sitter::transform::TreeBuilder, SyntaxNode};
+    use crate::{cst, tree_sitter::transform::transform_cst};
 
     #[test]
-    fn test() -> Result<(), String> {
+    fn test() {
         let input = r#"
 SELECT
 	1 as X
@@ -92,16 +82,10 @@ FROM
         // dbg!(input);
         let root = cst::parse(input).unwrap();
 
-        let mut tree_builder = TreeBuilder::new();
-        tree_builder.build(&root)?;
-
-        let (tree, interner) = tree_builder.finish();
-        let new_root = SyntaxNode::new_root_with_resolver(tree, interner);
+        let new_root = transform_cst(&root);
 
         // dbg!(&root);
         // dbg!(&new_root);
-        assert_eq!(format!("{root}"), format!("{new_root}"));
-
-        Ok(())
+        assert_eq!(format!("{root}"), format!("{new_root}")); // 書き換え前なので、同一のTreeになることを確認
     }
 }
